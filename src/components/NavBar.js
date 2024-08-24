496c5fbf6fbd502ceccbee97ef994ced4e820572
JoC204 <jbarrionuevo204@gmail.com>
2024-08-24 09:02:59 -0300

    Agregué iconos a NavBar: WhatsApp, Envelope, Phone

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,37 +1,38 @@
 // Navbar.js
-import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faHome, faUser, faCog } from '@fortawesome/free-solid-svg-icons';
-import DropdownMenu from './DropdownMenu';
-import '../style/NavBar.css';
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import DropdownMenu from "./DropdownMenu";
+import "../style/NavBar.css";
 
 const Navbar = () => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-    };
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
-    const closeMenu = () => {
-        setIsOpen(false);
-    };
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
 
-    return (
-        <div className='navBox'>
-            <nav className="navbar">
-                <div className="navbar-logo">BayMudanzas</div>
-                <div className="navbar-hamburger" onClick={toggleMenu}>
-                    <FontAwesomeIcon icon={faBars} />
-                </div>
-                <div className="navbar-links">
-                    <FontAwesomeIcon icon={faHome} />
-                    <FontAwesomeIcon icon={faUser} />
-                    <FontAwesomeIcon icon={faCog} />
-                </div>
-            </nav>
-                <DropdownMenu isOpen={isOpen} closeMenu={closeMenu} />
+  return (
+    <div className="navBox">
+      <nav className="navbar">
+        <div className="navbar-logo">BayMudanzas</div>
+        <div className="navbar-hamburger" onClick={toggleMenu}>
+          <FontAwesomeIcon icon={faBars} />
         </div>
-    );
+        <div className="navbar-links">
+          <FontAwesomeIcon icon={faPhone} />
+          <FontAwesomeIcon icon={faEnvelope} />
+          <FontAwesomeIcon icon={faWhatsapp} />
+        </div>
+      </nav>
+      <DropdownMenu isOpen={isOpen} closeMenu={closeMenu} />
+    </div>
+  );
 };
 
 export default Navbar;

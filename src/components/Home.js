7598225d1efef588e4bab352db5ca98df96aeb62
JoC204 <jbarrionuevo204@gmail.com>
2024-08-29 07:52:11 -0300

    Se agregó img de guardamuebles en home.js
    Falta diseñar la de pc

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,43 @@
 import React, { useEffect, useState } from "react";
 import PortadaMudanzaDesktop from "../img/mudanza-desktop.png";
 import PortadaMudanzaMobile from "../img/mudanza-mobile.png";
+// import PortadaGuardaDesktop from "../img/guardamuebles-desktop.png";
+import PortadaGuardaMobile from "../img/guardamuebles-mobile.png";
 import "../style/Home.css";
 
 const Home = () => {
   const [showImage, setShowImage] = useState(false);
 
   useEffect(() => {
-    setTimeout(() => setShowImage(true), 100); // Imagen aparece 2 segundos después del texto
+    setTimeout(() => setShowImage(true), 100);
   }, []);
 
   return (
     <div className="home-container">
-      <img
-        className={`home-image desktop ${showImage ? "show" : ""}`}
-        src={PortadaMudanzaDesktop}
-        alt="back"
-      />
-      <img
-        className={`home-image mobile ${showImage ? "show" : ""}`}
-        src={PortadaMudanzaMobile}
-        alt="back"
-      />
+      <div className="mudanza-section">
+        <img
+          className={`home-image desktop ${showImage ? "show" : ""}`}
+          src={PortadaMudanzaDesktop}
+          alt="Mudanza"
+        />
+        <img
+          className={`home-image mobile ${showImage ? "show" : ""}`}
+          src={PortadaMudanzaMobile}
+          alt="Mudanza"
+        />
+      </div>
+      <div className="guarda-section">
+        <img
+          className={`home-image desktop ${showImage ? "show" : ""}`}
+          // src={PortadaGuardaDesktop}
+          alt="Guardamuebles"
+        />
+        <img
+          className={`home-image mobile ${showImage ? "show" : ""}`}
+          src={PortadaGuardaMobile}
+          alt="Guardamuebles"
+        />
+      </div>
     </div>
   );
 };

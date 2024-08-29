84a4165b5a7b2fee0c40df4107f3b7427a6f0ee0
JoC204 <jbarrionuevo204@gmail.com>
2024-08-29 06:54:47 -0300

    En el componente Home.js portada responsive
    Se agregó una img para mobile y otra para PC.
    Mejorar Home.css y agregar img de guardamuebles en home.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,27 @@
 import React, { useEffect, useState } from "react";
-import ImgCamion from "../img/camion.png";
+import PortadaMudanzaDesktop from "../img/mudanza-desktop.png";
+import PortadaMudanzaMobile from "../img/mudanza-mobile.png";
 import "../style/Home.css";
 
 const Home = () => {
-  const [showTitle, setShowTitle] = useState(false);
-  const [showText, setShowText] = useState(false);
   const [showImage, setShowImage] = useState(false);
 
   useEffect(() => {
-    setTimeout(() => setShowTitle(true), 500); // Título aparece después de 0.5s
-    setTimeout(() => setShowText(true), 1000); // Texto aparece 2 segundos después del título
-    setTimeout(() => setShowImage(true), 1500); // Imagen aparece 2 segundos después del texto
+    setTimeout(() => setShowImage(true), 100); // Imagen aparece 2 segundos después del texto
   }, []);
 
   return (
     <div className="home-container">
-      <h1 className={`title ${showTitle ? "show" : ""}`}>MUDANZAS BAY</h1>
       <img
-        className={`home-image ${showImage ? "show" : ""}`}
-        src={ImgCamion}
-        alt="Camion"
+        className={`home-image desktop ${showImage ? "show" : ""}`}
+        src={PortadaMudanzaDesktop}
+        alt="back"
       />
-      <p className={`subtitle ${showText ? "show" : ""}`}>MUDANZA A MEDIDA</p>
-      <p className={`subtitle2 ${showText ? "show" : ""}`}>GUARDAMUEBLES</p>
+      <img
+        className={`home-image mobile ${showImage ? "show" : ""}`}
+        src={PortadaMudanzaMobile}
+        alt="back"
+      />
     </div>
   );
 };

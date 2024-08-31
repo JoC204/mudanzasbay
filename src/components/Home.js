d40ecee3060cd982bd57aa999880e5f1a43fde2d
JoC204 <jbarrionuevo204@gmail.com>
2024-08-31 19:27:04 -0300

    Add files
    Se agregó el archivo espesialistas.png, guarda-desk.png, mudanza-desktop.png, mudanza-mobile.png, ok.png y portada-home-mudanza-link.png.
    Se elimina manifiest.json de public y modifico index.html
    Se agregan las cards del centro de la página.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import PortadaMudanzaDesktop from "../img/mudanza-desktop.png";
+import Especialista from "../img/espesialistas.png";
+import Ok from "../img/ok.png";
 import PortadaMudanzaMobile from "../img/mudanza-mobile.png";
-// import PortadaGuardaDesktop from "../img/guardamuebles-desktop.png";
+import PortadaGuardaDesktop from "../img/guarda-desk.png";
 import PortadaGuardaMobile from "../img/guardamuebles-mobile.png";
 import "../style/Home.css";
 
@@ -26,10 +28,25 @@ const Home = () => {
           alt="Mudanza"
         />
       </div>
+      <div className="cards-section">
+        <div className="card">
+          <img src={Especialista} alt="Especialista"></img>
+          <h2>Somos especialistas</h2>
+          <p>Contamos con un equipo de expertos en mudanzas</p>
+        </div>
+        <div className="card">
+          <img src={Ok} alt="responsabilidad"></img>
+          <h2>Responsabilidad</h2>
+          <p>
+            Entregamos todos los bienes en las mismas condiciones que los
+            recibimos
+          </p>
+        </div>
+      </div>
       <div className="guarda-section">
         <img
           className={`home-image desktop ${showImage ? "show" : ""}`}
-          // src={PortadaGuardaDesktop}
+          src={PortadaGuardaDesktop}
           alt="Guardamuebles"
         />
         <img
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -21,11 +21,13 @@ const Navbar = () => {
   return (
     <div className="navBox">
       <nav className="navbar">
-        <div><img src={Logo} alt="Logo" className="navbar-logoBay" /></div>
-        {/* <div className="navbar-logo">MudanzasBay</div>  */}
+        <div className="navbar-logoBox">
         <div className="navbar-hamburger" onClick={toggleMenu}>
           <FontAwesomeIcon icon={faBars} />
         </div>
+          <img src={Logo} alt="Logo" className="navbar-logoBay" />
+        </div>
+        {/* <div className="navbar-logo">MudanzasBay</div>  */}
         <div className="navbar-links">
           <FontAwesomeIcon icon={faPhone} />
           <FontAwesomeIcon icon={faEnvelope} />

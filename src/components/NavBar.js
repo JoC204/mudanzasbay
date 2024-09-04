4395aa5359aa00deb343270b061ee60543e9939f
JoC204 <jbarrionuevo204@gmail.com>
2024-09-03 22:42:02 -0300

    Change NavBar
    Cambio de whatsapp a instagram
    se agregan links para cada icono
    Cambiado el color de los links a naranja
    Cambian de tama;o a los 1000px el logo y los icono.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,9 @@
 // Navbar.js
 import React, { useState } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
-import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import DropdownMenu from "./DropdownMenu";
 import Logo from "../img/logoBay.png";
 import "../style/NavBar.css";
@@ -23,36 +23,54 @@ const Navbar = () => {
     <div className="navBox">
       <nav className="navbar">
         <div className="navbar-logoBox">
-        <div className="navbar-hamburger" onClick={toggleMenu}>
-          <FontAwesomeIcon icon={faBars} />
-        </div>
-        <Link to="/">
-          <img src={Logo} alt="Logo" className="navbar-logoBay" />
-        </Link>  
+          <div className="navbar-hamburger" onClick={toggleMenu}>
+            <FontAwesomeIcon icon={faBars} />
+          </div>
+          <Link to="/">
+            <img src={Logo} alt="Logo" className="navbar-logoBay" />
+          </Link>
         </div>
 
         <div className="links">
-            <ul>
-                <li>
-                    <Link className='link' to="/" onClick={closeMenu}>Home</Link>
-                </li>
-                <li>
-                    <Link className='link' to="/mudanzas" onClick={closeMenu}>Fletes/Mudanzas</Link>
-                </li>
-                <li>
-                    <Link className='link' to="/guardamuebles" onClick={closeMenu}>Guardamuebles</Link>
-                </li>
-                <li>
-                    <Link className='link' to="/contacto" onClick={closeMenu}>Contacto</Link>
-                </li>
-            </ul>
+          <ul>
+            <li>
+              <Link className="link" to="/" onClick={closeMenu}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className="link" to="/mudanzas" onClick={closeMenu}>
+                Fletes/Mudanzas
+              </Link>
+            </li>
+            <li>
+              <Link className="link" to="/guardamuebles" onClick={closeMenu}>
+                Guardamuebles
+              </Link>
+            </li>
+            <li>
+              <Link className="link" to="/contacto" onClick={closeMenu}>
+                Contacto
+              </Link>
+            </li>
+          </ul>
         </div>
 
-        {/* <div className="navbar-logo">MudanzasBay</div>  */}
         <div className="navbar-links">
-          <FontAwesomeIcon icon={faPhone} />
-          <FontAwesomeIcon icon={faEnvelope} />
-          <FontAwesomeIcon icon={faWhatsapp} />
+          <a href="tel:+5493416541371" className="icon-link">
+            <FontAwesomeIcon icon={faPhone} />
+          </a>
+          <a href="mailto:fletebay@gmail.com" className="icon-link">
+            <FontAwesomeIcon icon={faEnvelope} />
+          </a>
+          <a
+            href="https://www.instagram.com/bay.mudanzas/"
+            className="icon-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
         </div>
       </nav>
       <DropdownMenu isOpen={isOpen} closeMenu={closeMenu} />

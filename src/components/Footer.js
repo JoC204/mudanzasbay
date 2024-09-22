1a67c7fb788aad6ab89a688baadd75add634ba85
JoC204 <jbarrionuevo204@gmail.com>
2024-09-22 00:07:37 -0300

    Iconos NavBar
    Se agregó hover a los iconos del NavBar
    Se eliminó componente carousel y su css
    se agrega png para el logo de instagram.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,27 @@
 // src/components/Footer.js
-import React from 'react';
-import '../style/Footer.css'; // Asegúrate de tener este archivo para los estilos
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import "../style/Footer.css"; // Asegúrate de tener este archivo para los estilos
 
 const Footer = () => {
   return (
     <footer className="footer-container">
+      <a href="tel:+5493416541371" className="icon-link">
+        <FontAwesomeIcon icon={faPhone} />
+      </a>
+      <a href="mailto:fletebay@gmail.com" className="icon-link">
+        <FontAwesomeIcon icon={faEnvelope} />
+      </a>
+      <a
+        href="https://www.instagram.com/bay.mudanzas/"
+        className="icon-link"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faInstagram} />
+      </a>
       <hr className="footer-divider" />
       <p>Todos los derechos reservados</p>
     </footer>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faEnvelope, faEnvelopeOpen, faPhone } from "@fortawesome/free-solid-svg-icons";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import DropdownMenu from "./DropdownMenu";
 import Logo from "../img/logoBay.png";
+import LogoHover from "../img/insta-color.png"; // Logo alternativo
 import "../style/NavBar.css";
 
 const Navbar = () => {
@@ -20,24 +21,21 @@ const Navbar = () => {
     setIsOpen(false);
   };
 
-  // Hook para controlar la visibilidad del Navbar al hacer scroll
-  const handleScroll = () => {
-    const currentScrollPos = window.pageYOffset;
+ // Hook para controlar la visibilidad del Navbar al hacer scroll
+ const handleScroll = useCallback(() => {
+  const currentScrollPos = window.pageYOffset;
 
-    // Si el usuario desplaza hacia abajo, oculta el navbar. Si desplaza hacia arriba, muéstralo.
-    if (prevScrollPos > currentScrollPos) {
-      setVisible(true);
-    } else {
-      setVisible(false);
-    }
-
-    setPrevScrollPos(currentScrollPos);
-  };
-
+  // Usa la función anterior para obtener el valor más reciente
+  setVisible(prevVisible => {
+    return prevScrollPos > currentScrollPos; // Aquí puedes comparar directamente
+  });
+  setPrevScrollPos(currentScrollPos);
+}, [prevScrollPos]); // Sin dependencias aquí
+  
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
-  }, [prevScrollPos]);
+  }, [handleScroll]);
 
   return (
     <div className={`navBox ${visible ? "visible" : "hidden"}`}>
@@ -46,7 +44,7 @@ const Navbar = () => {
           <div className="navbar-hamburger" onClick={toggleMenu}>
             <FontAwesomeIcon icon={faBars} />
           </div>
-          <Link to="/">
+          <Link to="/" className="navbar-logo">
             <img src={Logo} alt="Logo" className="navbar-logoBay" />
           </Link>
         </div>
@@ -77,11 +75,12 @@ const Navbar = () => {
         </div>
 
         <div className="navbar-links">
-          <a href="tel:+5493416541371" className="icon-link">
+          <a href="tel:+5493416541371" className="icon-link" id="phone">
             <FontAwesomeIcon icon={faPhone} />
           </a>
           <a href="mailto:fletebay@gmail.com" className="icon-link">
-            <FontAwesomeIcon icon={faEnvelope} />
+            <FontAwesomeIcon icon={faEnvelope} className="icon-envelope" />
+            <FontAwesomeIcon icon={faEnvelopeOpen} className="icon-envelope-open" />
           </a>
           <a
             href="https://www.instagram.com/bay.mudanzas/"
@@ -89,7 +88,8 @@ const Navbar = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <FontAwesomeIcon icon={faInstagram} />
+            <FontAwesomeIcon icon={faInstagram} className="icon-instagram logo-default" />
+            <img src={LogoHover} alt="Logo Hover" className="logo-hover" />
           </a>
         </div>
       </nav>

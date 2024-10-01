99efe577ed5b84fb3bd1788fb91344ec87ec1d30
JoC204 <jbarrionuevo204@gmail.com>
2024-09-30 22:27:27 -0300

    Se agrega salto de linea y dos guiones medios para diferenciar el texto.

--- src/components/WhatsAppButton.js
+++ src/components/WhatsAppButton.js
@@ -1,11 +1,11 @@
 import React from "react";
-import whatsappIconUno from "../img/whatsApp.png"; // Asegúrate de corregir la ruta según la ubicación del ícono
+import whatsappIconUno from "../img/whatsApp.png";
 import "../style/WhatsAppButton.css";
 
 const WhatsAppButton = () => {
   return (
     <a
-      href="https://wa.me/+5493416541371?text=Hola%2C%20te%20contacto%20desde%20la%20web%20de%20MudanzasBay%0A%20--%20"
+      href="https://wa.me/+5493416541371?text=Hola%2C%20te%20contacto%20desde%20la%20web%20de%20MudanzasBay%0A%0A%20--"
       target="_blank"
       rel="noopener noreferrer"
       style={styles.container}
